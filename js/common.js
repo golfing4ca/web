cdf9e5087a64892993799f42c97213656e172fc7
bigd1182 <bryant.derrill@gmail.com>
2016-07-14 19:19:54 -0400

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,1 +1,22 @@
+//this function includes all necessary js files for the application
+function include(file)
+{
 
+  var script  = document.createElement('script');
+  script.src  = file;
+  script.type = 'text/javascript';
+  script.defer = true;
+
+  document.getElementsByTagName('head').item(0).appendChild(script);
+
+}
+
+/* include any js files here */
+include('//ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js');
+include('//rawgit.com/golfing4ca/s3ShareIt/master/js/s3ShareIt.js');
+
+/*
+<script src="//ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" data-pagecloud-original-type="text/javascript" type="disabled" data-runtime-script="true"></script>
+
+<script src="//rawgit.com/golfing4ca/s3ShareIt/master/js/s3ShareIt.js" type="disabled" data-runtime-script="true"></script>
+*/
