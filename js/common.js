34b9e143a6bb2c0a144844bdffdb9322a79000b6
bigd1182 <bryant.derrill@gmail.com>
2016-07-14 19:56:56 -0400

    Update common.js

--- js/common.js
+++ js/common.js
@@ -3,7 +3,7 @@ jQuery.cachedScript = function( url, options ) {
     // Allow user to set any option except for dataType, cache, and url
     options = $.extend( options || {}, {
         dataType: "script",
-        cache: true,
+        cache: false,
         url: url
     });
 
