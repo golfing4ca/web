095a3a8903f995d6e595349d0eb223958d2e1c61
bigd1182 <bryant.derrill@gmail.com>
2016-07-14 20:07:20 -0400

    Update and rename master.js to master_test.js

---
+++ js/master_test.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function($){
+ 
+ /**
+  * function to load a given css file 
+  */ 
+ loadCSS = function(href) {
+     var cssLink = $("<link rel='stylesheet' type='text/css' href='"+href+"'>");
+     $("head").append(cssLink); 
+ };
+
+/**
+ * function to load a given js file 
+ */ 
+ loadJS = function(src) {
+     var jsLink = $("<script type='text/javascript' src='"+src+"'>");
+     $("head").append(jsLink); 
+ }; 
+  
+ // load the css file 
+ loadCSS("//cdn.rawgit.com/golfing4ca/web/js/style.css");
+
+ // load the js file 
+ loadJS("//cdn.rawgit.com/golfing4ca/web/js/one.js");
+
+});
