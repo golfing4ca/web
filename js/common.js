745a24763d68c4314e5828eda106ccd6ea582dcd
bigd1182 <bryant.derrill@gmail.com>
2016-08-02 18:12:40 -0400

    Update common.js

--- js/common.js
+++ js/common.js
@@ -4,8 +4,10 @@ $(function(){
   $.get('/__menu', function(result){
     $result = $(result);
     
-    $result.find('body').appendTo('#header');
+    $result.find('link').appendTo('#header');
+    $result.find('style').appendTo('#header');
     $result.find('script').appendTo('#header');
+    $result.find('body').appendTo('#header');
   }, 'html');
 
 });
