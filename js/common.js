82bc9010233c9274ee0ff79e301c68b9425cf4a2
bigd1182 <bryant.derrill@gmail.com>
2016-08-02 18:24:34 -0400

    Update common.js

--- js/common.js
+++ js/common.js
@@ -4,10 +4,10 @@ $(function(){
   $.get('//golfing4ca.org/__menu', function(result){
     $result = $(result);
     
-    $result.find('link').appendTo('head');
-    $result.find('style').appendTo('head');
+    //$result.find('link').appendTo('head');
+    //$result.find('style').appendTo('head');
     $result.find('script').appendTo('head');
-    $result.find('body').appendTo('#header');
+    //$result.find('body').appendTo('#header');
   }, 'html');
 
 });
