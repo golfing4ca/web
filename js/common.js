f8cf7f098de797916510e0d8818a2ed8bee0c800
bigd1182 <bryant.derrill@gmail.com>
2016-08-05 13:15:34 -0400

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,13 +1,19 @@
-//Load Header
-$(function(){
-
-  $.get('//golfing4ca.org/__menu', function(result){
-    $result = $(result);
-    
-    //$result.find('link').appendTo('head');
-    //$result.find('style').appendTo('head');
-    $result.find('script').appendTo('head');
-    //$result.find('body').appendTo('#header');
-  }, 'html');
-
-});
+/*Load Common Scripts
+<HEAD>: document.head.appendChild(node);
+<BODY>: document.body.appendChild(node);
+*/
+/*
+//jQuery
+var node = document.createElement('script');
+    node.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js";
+    document.body.appendChild(node);
+*/
+//Navigation Header
+var node = document.createElement('script');
+    node.type = type="text/javascript";
+    node.src = "//cdn.golfing4ca.org/golfing4ca/s3ShareIt/master/js/s3ShareIt.js";
+    document.body.appendChild(node);
+var node = document.createElement('script');
+    node.type = type="text/javascript";
+    node.src = "https://s3.amazonaws.com/menumaker/menumaker.min.js";
+    document.body.appendChild(node);
