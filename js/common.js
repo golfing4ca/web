fb02583c6e629e5fed1493eb23052b657fa3913a
bigd1182 <bryant.derrill@gmail.com>
2016-08-09 13:53:31 -0400

    Update common.js

--- js/common.js
+++ js/common.js
@@ -23,3 +23,10 @@ common$('head').append('<script src="https://cdn.golfing4ca.org/golfing4ca/s3Sha
 common$('head').append('<script src="https://s3.amazonaws.com/menumaker/menumaker.min.js"></script>');
 common$('head').append('<script src="https://load.sumome.com/" data-sumo-site-id="12ee2b1c2563d228da54eed4f7s09ff32f5aea5ecba20d58c66580fea4d1cdf70" async="async"></script>');
 }
+function preload(arrayOfImages) {
+$(arrayOfImages).each(function() {
+  $('<img/>')[0].src = this;
+  // Alternatively you could use:
+  // (new Image()).src = this;
+});
+}
